e536bd10791801384a33e790b1f0240b8d9d6bd5
Samsuffi <shumbahuur@hotmail.fr>
2017-05-12 15:38:01 +0200

    création d'une feuille de style

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
   <title>Accueil</title>
 </head>
 <body>
+	<?php include "header.php" ?>
   <h1>Une petite page d'accueil</h1>
   <p>Cependant, ils ont le droit de se exprimer par tous les moyens, même irrationnelle, pour prouver leur folie</p>
   <p>Celui qui travaille pour un salaire, d'autre part, a peu d'incitations à travailler</p>
   <p>Ils sont des nations dont le nationalisme a été détruit et qui sont ainsi déchirée</p>
   <p>La richesse d'une société peut être assimilée à un établissement d'alimentation ou un magasin offrant un certain nombre de personnes avec des rations quotidiennes satisfaire leurs besoins</p>
+	<p>code généré via <a href="http://www.gaddafipsum.com/">Gaddafipsum.com</a><p>
 </body>
 </html>
